bea9ef05955bda216b53c27fe42c536db55a6285
jmrobinsonma <jmrobinsonma@gmail.com>
2020-07-03 09:17:12 -0400

    v5

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_wtf.csrf import CsrfProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+#from oauthlib.oauth2 import WebApplicationClient
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "0d9ffa033dba18831e5575a9e8d25580"
@@ -12,6 +13,14 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 bcrypt = Bcrypt(app)
+
+# Configuration
+#GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+#GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+#GOOGLE_DISCOVERY_URL = (
+#    "https://accounts.google.com/.well-known/openid-configuration"
+#)
+
 
 from app import routes
 
