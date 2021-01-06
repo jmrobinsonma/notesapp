ed651a69d6f36e159e1dfb4594962450f61b7322
jmrobinsonma <jmrobinsonma@gmail.com>
2021-01-05 19:57:32 -0500

    removed weather widget from login. Changed username and password

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SECRET_KEY'] = "0d9ffa033dba18831e5575a9e8d25580" #os.getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///notes.db" #os.getenv("SQLALCHEMY_DATABASE_URI")
 
 db = SQLAlchemy(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
--- app/models.py
+++ app/models.py
@@ -16,9 +16,9 @@ class Notes(db.Model):
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
-	email = db.Column(db.String(120), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=False, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
 
 	def __repr__(self):
-		return f"{self.username} {self.email}"
+		return f"{self.id} {self.username} {self.email} {self.password}"
 		
